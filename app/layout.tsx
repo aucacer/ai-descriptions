3cc7878b936d702a57fd899cb5d4d5206f50e2d9
aucacer <alex91caceres@gmail.com>
2025-07-12 07:15:25 +1000

    added google adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google"
 import "./globals.css"
 import { Toaster } from "@/components/ui/toaster"
 import { Analytics } from "@vercel/analytics/next"
+import Script from "next/script"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -21,6 +22,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3785282145645278"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body className={inter.className}>
         {children}
         <Toaster />
